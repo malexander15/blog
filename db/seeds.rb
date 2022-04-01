1c83b4c2a1e962914a292495bf594aba0dbe51d7
malexander15 <creepinjezus15@gmail.com>
2022-04-01 11:54:05 -0400

    update users with sessions and user names. permissions not working? signed in users can still edit others users posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
- 
+ User.create(email: "maxx@example.com", password: "password", password_confirmation: "password")
+
+ 10.times do |x|
+ 	Post.create(title: "Title #{x}", body: "Body #{x}", user_id: User.first.id)
+ end
